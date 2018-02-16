c2b4fea6471d649e5b4cc7839589b03e2b6f7dfb
Michael Losada <michael.losada@gmail.com>
2018-02-16 12:50:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(chocolateBars, element){
+  ["foo", ...chocolateBars]
+  return chocolateBars;
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+chocolateBars.unshift("foo")
+ return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, element){
+  [chocolateBars, ..."foo"]
+  return chocolateBars
+}
+
+function destructivelyAddElementToEndOfArray(){
+  chocolateBars.push("foo")
+  return chocolateBars
+}
+
+function accessElementInArray(chocolateBars,index){
+  return chocolateBars[index]
+}
